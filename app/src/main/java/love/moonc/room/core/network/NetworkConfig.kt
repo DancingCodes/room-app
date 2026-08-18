14426db400cb3129a375831f8655a6fdd1b5dd8c
DANCINGCODES\zhang <dancingcodes@qq.com>
2026-08-18 13:37:34 +0800

    1

--- app/src/main/java/love/moonc/room/core/network/NetworkConfig.kt
+++ app/src/main/java/love/moonc/room/core/network/NetworkConfig.kt
@@ -1,6 +1,6 @@
 package love.moonc.room.core.network
 
 object NetworkConfig {
-    const val BACKEND_BASE_URL = "http://10.0.2.2:8080/"
+    const val BACKEND_BASE_URL = "http://192.168.110.143:8080/"
     const val ENABLE_HTTP_LOGGING = true
 }
