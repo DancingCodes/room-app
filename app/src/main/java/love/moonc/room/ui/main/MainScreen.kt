7f8ea0919b0bfb014717826e3db06ba1db388cf9
DANCINGCODES\zhang <dancingcodes@qq.com>
2026-08-18 17:12:10 +0800

    1

--- app/src/main/java/love/moonc/room/ui/main/MainScreen.kt
+++ app/src/main/java/love/moonc/room/ui/main/MainScreen.kt
@@ -1,6 +1,12 @@
 package love.moonc.room.ui.main
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.AsyncImage
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
