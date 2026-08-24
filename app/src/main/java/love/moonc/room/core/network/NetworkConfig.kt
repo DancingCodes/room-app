5a87f1645320479e765c193a6d9ecb1c5d78cd38
DANCINGCODES\zhang <dancingcodes@qq.com>
2026-08-24 10:48:23 +0800

    1

--- app/src/main/java/love/moonc/room/core/network/NetworkConfig.kt
+++ app/src/main/java/love/moonc/room/core/network/NetworkConfig.kt
@@ -2,6 +2,5 @@ package love.moonc.room.core.network
 
 object NetworkConfig {
     const val BACKEND_BASE_URL = "https://room.moonc.love/"
-    // const val BACKEND_BASE_URL = "http://192.168.110.143:8080/"
     const val AGORA_APP_ID = "cca0b8f4bd794b95b0479667be7c9cff"
 }
